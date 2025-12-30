ef73fdd82ce8de2675ecf6a45385d651516d472b
ThunderCoreDev <castellashly0@gmail.com>
2025-12-30 07:50:05 -0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,9 +10,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
     }
 }
 
 rootProject.name = "ThunderTools"
-include(":app")
+include(":app")  # ← ¡ESTA LÍNEA ES CRÍTICA!
