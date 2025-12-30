dc900888dfac4dcffd13db3fbfa1d8c96556927f
ThunderCoreDev <castellashly0@gmail.com>
2025-12-30 07:52:17 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,7 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// EMPTY or just plugins
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
     id("org.jetbrains.kotlin.kapt") version "1.9.10" apply false
 }
-
-// ¡COMENTA O ELIMINA ESTE BLOQUE COMPLETO!
-// tasks.register("clean", Delete::class) {
-//     delete(rootProject.buildDir)
-// }
